e6b657838660464cdcd9ad9470c32938f1d8b814
WONG SIN YAW <ICT_3337922K@connect.np.edu.sg>
2024-12-31 16:15:57 +0800

    rename and add test.js

---
+++ practical2/books-api-db/sum.test.js
@@ -0,0 +1,8 @@
+// filepath: /c:/Users/wsysw/Desktop/Diploma/Server-Side Development/SSDOct2024_Assignment_SinYaw/sum.test.js
+function sum(a, b) {
+  return a + b;
+}
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
